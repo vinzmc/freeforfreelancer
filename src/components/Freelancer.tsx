a4492dc2d59840648c1abbcdb4b9844dd4a26771
vinzmc <maurice.marvin@student.umn.ac.id>
2021-10-22 16:22:52 +0700

    Benerin bintang review

--- src/components/Freelancer.tsx
+++ src/components/Freelancer.tsx
@@ -1,9 +1,10 @@
 import { IonAvatar, IonIcon } from "@ionic/react"
-import { star } from "ionicons/icons"
+import { star, starOutline } from "ionicons/icons"
 
 import './Freelancer.css'
 
 const Freelancer: React.FC<{ name: string, job: string, star: number, review: number, price: string, pic: string }> = props => {
+    var starBlank = 5 - props.star;
     return (
         <div className="freelancer">
 
@@ -23,7 +24,14 @@ const Freelancer: React.FC<{ name: string, job: string, star: number, review: nu
                         <IonIcon icon={star} />
                         <IonIcon icon={star} />
                     </div> */}
-                    <h2 className="review-qty">{props.star} stars - ({props.review} reviews)</h2>
+                    <h2 className="review-qty">
+                        {props.star !== 0 && [...Array(props.star)].map((x, i) =>
+                            <IonIcon icon={star} key={i} />
+                        )}
+                        {starBlank !== 0 && [...Array(starBlank)].map((x, i) =>
+                            <IonIcon icon={starOutline} key={5 - i} />
+                        )} ({props.review} reviews)
+                    </h2>
                 </div>
             </div>
 
