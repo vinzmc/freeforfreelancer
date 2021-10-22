44e0d520079eacb77d94544ccff7605f35295e79
sergio5535 <sergionathaniel5535@gmail.com>
2021-10-22 15:59:43 +0700

    search card
    display search card, database from firebase 'freelancer'

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,17 +1,45 @@
-import { IonBackButton, IonButtons, IonContent, IonLabel, IonPage, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import { IonBackButton, IonButton, IonButtons, IonCard, IonContent, IonItem, IonLabel, IonList, IonPage, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import firebase from '../firebase';
+import Freelancer from "../components/Freelancer";
+import './SearchPage.css'
 
 const SearchPage: React.FC = () => {
+    const [data, setData] = useState<any>([]);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection('freelancer')
+            .onSnapshot((snapshot) => {
+                const newData = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }))
+
+                setData(newData);
+            })
+    }, [])
+
     return (
         <IonPage>
             <IonToolbar className="ion-margin-top">
                 <IonTitle className="titleMiddle">Search</IonTitle>
                 <IonButtons slot="start">
-                    <IonBackButton defaultHref="/HomePage"/>
+                    <IonBackButton defaultHref="/HomePage" />
                 </IonButtons>
             </IonToolbar>
             <IonContent>
-                <IonSearchbar placeholder="Designer"></IonSearchbar>
+                <IonSearchbar style={{marginBottom: '2rem'}} placeholder="Designer"></IonSearchbar>
+                <IonLabel className="label">6 Freelancers Found</IonLabel>
+                <IonList>
+                    {data.map((doc: any) =>
+                        <IonItem key={doc.id} href='/ProfilePage'>
+                            <Freelancer name={doc.name} job={doc.job} star={doc.star} review={doc.review} price={doc.price + 'M'} pic={doc.pic} />
+                        </IonItem>
+                        )
+                    }
+                </IonList>
             </IonContent>
         </IonPage>
     );
