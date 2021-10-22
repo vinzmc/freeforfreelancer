576ddc195805c09e75e07f6dd28e4f2258f6b829
vinzmc <maurice.marvin@student.umn.ac.id>
2021-10-22 17:12:12 +0700

    searchbar logic

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,13 @@
-import { IonBackButton, IonButton, IonButtons, IonCard, IonContent, IonItem, IonLabel, IonList, IonPage, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react";
+import { IonContent, IonLabel, IonList, IonPage, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react";
 import React, { useEffect, useState } from "react";
 import firebase from '../firebase';
 import Freelancer from "../components/Freelancer";
 import './SearchPage.css'
 
 const SearchPage: React.FC = () => {
+    const [searchText, setSearchText] = useState('');
     const [data, setData] = useState<any>([]);
+    const [renderedData, setRenderedData] = useState<any>([]);
 
     useEffect(() => {
         firebase
@@ -18,23 +20,33 @@ const SearchPage: React.FC = () => {
                 }))
 
                 setData(newData);
+                setRenderedData(newData);
             })
     }, [])
 
+    function search(searchText: string) {
+        setSearchText(searchText);
+        const newData = data.filter((value: any) => {
+            return value.name.toLowerCase().includes(searchText.toLowerCase()) ||
+                value.job.toLowerCase().includes(searchText.toLowerCase());
+        })
+        setRenderedData(newData);
+    }
+
     return (
         <IonPage>
-            <IonToolbar className="ion-margin-top">
-                <IonTitle className="titleMiddle">Search</IonTitle>      
-            </IonToolbar>
             <IonContent>
-                <IonSearchbar style={{marginBottom: '2rem'}} placeholder="Designer"></IonSearchbar>
-                <IonLabel className="label">6 Freelancers Found</IonLabel>
+                <IonToolbar className="ion-margin-top">
+                    <IonTitle className="titleMiddle">Search</IonTitle>
+                </IonToolbar>
+                <IonSearchbar style={{ marginBottom: '2rem' }} placeholder="Designer, Programmer..." onIonChange={e => search(e.detail.value!)} value={searchText} showCancelButton="focus"></IonSearchbar>
+                <IonLabel className="label">{renderedData.length} Freelancers Found</IonLabel>
                 <IonList>
-                    {data.map((doc: any) =>
-                        <div key={doc.id} >
+                    {renderedData.map((doc: any) =>
+                        <div key={doc.id}>
                             <Freelancer key={doc.id} name={doc.name} job={doc.job} star={doc.star} review={doc.review} price={doc.price + 'M'} pic={doc.pic} />
                         </div>
-                        )
+                    )
                     }
                 </IonList>
             </IonContent>
