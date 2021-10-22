de8c764b46ad4604a6baad46f1577d4b6f78e7fd
sergio5535 <sergionathaniel5535@gmail.com>
2021-10-22 14:18:16 +0700

    Create SearchPage.tsx

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,12 @@
+import { IonPage, IonTitle } from "@ionic/react";
+import React from "react";
+
+const SearchPage: React.FC = () => {
+    return(
+        <IonPage>
+            <IonTitle>adasda</IonTitle>
+        </IonPage>
+    );
+}
+
+export default SearchPage;
