cd2264139acb5efafac7295c55a06838487b0084
Kaleb Juliu <kalebjuliu@gmail.com>
2021-10-22 23:03:04 +0700

    payment page

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 //theme
 import './ProfilePage.css';
+import './Payment.css'
 import { star, starOutline, location } from 'ionicons/icons';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
@@ -16,7 +17,7 @@ const Payment: React.FC = () => {
     const uriData = useParams<any>();
     const [showModal, setShowModal] = useState(false);
 
-    const payment = (id:string) =>{
+    const payment = (id: string) => {
         window.location.href = "/Payment/Freelancer/".concat(id);
         setShowModal(false);
     }
@@ -55,33 +56,30 @@ const Payment: React.FC = () => {
     return (
         <IonPage>
             <IonToolbar className="ion-margin-top">
-                    <IonTitle className="titleMiddle">Payment</IonTitle>
-                    <IonButtons slot="start">
-                        <IonBackButton defaultHref={`/Tabs/Freelancer/${uriData.id}`}/>
-                    </IonButtons>
+                <IonTitle className="titleMiddle" style={{ fontWeight: "500", fontSize: "16px" }}>Payment</IonTitle>
+                <IonButtons slot="start">
+                    <IonBackButton defaultHref={`/Tabs/Freelancer/${uriData.id}`} />
+                </IonButtons>
             </IonToolbar>
             <IonContent fullscreen>
-                <div className="profile-header-background">
-                    {/* <img></img> */}
-                </div>
                 {/* Profile Header Content */}
                 <div className="profile-header-content">
                     <IonGrid>
                         <IonRow>
                             <IonCol size="2.2" className=" ion-padding">
-                                <IonAvatar className="profile-avatar">
+                                <IonAvatar className="profile-avatar" style={{ top: "5px" }}>
                                     <img src={data.pic} />
                                 </IonAvatar>
                             </IonCol>
                             <IonCol style={{ marginLeft: "1.5rem" }}>
                                 <div>
-                                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                                    <div style={{ display: "flex", justifyContent: "space-between" }}>
                                         <div>
-                                            <h1 className="ion-no-margin profile-name">{data.name}</h1>
-                                            <p className="ion-no-margin profile-desc">{data.job}</p>
+                                            <h1 className="ion-no-margin profile-name" style={{ fontSize: "14px" }}>{data.name}</h1>
+                                            <p className="ion-no-margin profile-desc" style={{ marginTop: "4px", color: "gray" }}>{data.job}</p>
                                         </div>
-                                        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", flexDirection: "column" }}>
-                                            <p className="ion-no-margin profile-desc">Rp. {data.price}M</p>
+                                        <div>
+                                            <p className="ion-no-margin profile-desc" style={{ marginRight: "12px" }}>Rp. {data.price}M</p>
                                         </div>
 
                                     </div>
@@ -107,17 +105,39 @@ const Payment: React.FC = () => {
                         </IonRow>
                     </IonGrid>
                 </div>
-                <IonItem>
-                    <IonLabel>Payment Method</IonLabel>
-                    <IonSelect value={payMethod} okText="Okay" cancelText="Dismiss" onIonChange={e => setPayMethod(e.detail.value)}>
-                        <IonSelectOption value="bca">BCA virtual account</IonSelectOption>
-                        <IonSelectOption value="mandiri">Mandiri virtual account</IonSelectOption>
-                        <IonSelectOption value="bni">BNI virtual account</IonSelectOption>
-                        <IonSelectOption value="btn">BTN virtual account</IonSelectOption>
-                        <IonSelectOption value="danamon">Danamon virtual account</IonSelectOption>
-                        <IonSelectOption value="cimb">CIMB virtual account</IonSelectOption>
-                    </IonSelect>
-                </IonItem>
+                <div className="summary-box ion-margin">
+                    <h3 className="summary-box-title">Summary</h3>
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <h3 className="summary-detail">Freelancer</h3>
+                        <h3 className="summary-price">Rp. {data.price} M</h3>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <h3 className="summary-detail">Extra Service Fee</h3>
+                        <h3 className="summary-price">Rp. 30,000</h3>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <h3 className="summary-detail">Total</h3>
+                        <h3 className="summary-price">Rp. xxxxxxx</h3>
+                    </div>
+                </div>
+
+                <div className="summary-box ion-margin">
+                    <h3 className="summary-box-title">Payment Method</h3>
+                    <IonItem lines="none">
+                        <IonLabel style={{ fontSize: "13px" }}>Payment Method</IonLabel>
+                        <IonSelect value={payMethod} okText="Okay" cancelText="Dismiss" onIonChange={e => setPayMethod(e.detail.value)}>
+                            <IonSelectOption value="bca">BCA virtual account</IonSelectOption>
+                            <IonSelectOption value="mandiri">Mandiri virtual account</IonSelectOption>
+                            <IonSelectOption value="bni">BNI virtual account</IonSelectOption>
+                            <IonSelectOption value="btn">BTN virtual account</IonSelectOption>
+                            <IonSelectOption value="danamon">Danamon virtual account</IonSelectOption>
+                            <IonSelectOption value="cimb">CIMB virtual account</IonSelectOption>
+                        </IonSelect>
+                    </IonItem>
+                </div>
+
+                <IonButton className="ion-padding summary-button" expand="full" >Confirm and Checkout</IonButton>
+
             </IonContent>
         </IonPage>
     );
