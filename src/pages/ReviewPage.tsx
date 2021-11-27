56c93830ce488a767f9bbf9712c3edf1a8cc6e94
carlosmax21 <carlosmaximiliano21@gmail.com>
2021-11-27 14:57:38 +0700

    feat: add review page

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -4,15 +4,38 @@ import { useEffect, useState } from 'react';
 
 //theme
 import { star, starOutline } from 'ionicons/icons';
+import { FaStar } from 'react-icons/fa'
+import './ReviewPage.css'
 
 
 const ReviewPage: React.FC = () => {
+    const [ rating, setRating ] = useState<number>(0);
+
     return (
         <IonPage>
             <IonToolbar className="ion-margin-top">
                 <IonTitle className="titleMiddle" style={{ fontWeight: "500", fontSize: "16px" }}>Review</IonTitle>
             </IonToolbar>
             <IonContent fullscreen>
+                <div className="ion-text-center">
+                    {[...Array(5)].map((star, i) => {
+                        const ratingValue = i + 1;
+
+                        return(
+                            <label>
+                                <input 
+                                    type="radio" 
+                                    name="rating"
+                                    value={ratingValue}
+                                    onClick={() => setRating(ratingValue)}
+                                    />
+                                <FaStar className="star" color={ratingValue <= rating ? "ffff00" : "7f7f7f"} size={50} />
+                            </label>
+                        );
+
+                    })}
+                </div>
+                <IonButton onClick={() => console.log(rating)}></IonButton>
             </IonContent>    
         </IonPage>
     );
