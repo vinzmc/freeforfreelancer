e6c3cd1cd0bf396b2010ed960055fe599ac50492
Kaleb Juliu <kalebjuliu@gmail.com>
2021-11-27 20:32:48 +0700

    Add review page styling

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonContent, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import { useState } from 'react';
 
 //theme
@@ -7,7 +7,7 @@ import './ReviewPage.css'
 
 
 const ReviewPage: React.FC = () => {
-    const [ rating, setRating ] = useState<number>(0);
+    const [rating, setRating] = useState<number>(0);
 
     return (
         <IonPage>
@@ -15,26 +15,37 @@ const ReviewPage: React.FC = () => {
                 <IonTitle className="titleMiddle" style={{ fontWeight: "500", fontSize: "16px" }}>Review</IonTitle>
             </IonToolbar>
             <IonContent fullscreen>
-                <div className="ion-text-center">
-                    {[...Array(5)].map((star, i) => {
-                        const ratingValue = i + 1;
 
-                        return(
-                            <label>
-                                <input 
-                                    type="radio" 
-                                    name="rating"
-                                    value={ratingValue}
-                                    onClick={() => setRating(ratingValue)}
+                <div className="summary-box ion-margin">
+                    <div className="ion-text-center">
+                        <div>
+                            <p>Whats your review?</p>
+                        </div>
+                        {[...Array(5)].map((star, i) => {
+                            const ratingValue = i + 1;
+
+                            return (
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="rating"
+                                        value={ratingValue}
+                                        onClick={() => setRating(ratingValue)}
                                     />
-                                <FaStar className="star" color={ratingValue <= rating ? "ffff00" : "7f7f7f"} size={50} />
-                            </label>
-                        );
-
-                    })}
+                                    <FaStar className="star" color={ratingValue <= rating ? "ffc107" : "7f7f7f"} size={20} />
+                                </label>
+                            );
+                        })}
+                    </div>
+                    <div className="ion-margin-top">
+                        <div>
+                            <IonTextarea placeholder="Write your experience with this freelancer....." className="form-input" rows={6} style={{ color: "gray" }}></IonTextarea>
+                        </div>
+                        <IonButton className="summary-button ion-margin-top" onClick={() => console.log(rating)}>Review</IonButton>
+                    </div>
                 </div>
-                <IonButton onClick={() => console.log(rating)}></IonButton>
-            </IonContent>    
+
+            </IonContent>
         </IonPage>
     );
 };
